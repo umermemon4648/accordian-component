6e8296f13c7b271aba769218d0c7e0e50942a007
umermemon4648 <umermemon4648@gmail.com>
2023-05-11 13:02:44 +0500

    feature completed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const allAccordian = document.querySelectorAll('.accordian')
-allAccordian.forEach((accord)=>{
-    const icons =  document.querySelector('.icons')
-    const answers =  document.querySelector('.answer')
+allAccordian.forEach(accord =>{
+    const icons =  accord.querySelector('.icons')
+    const answers =  accord.querySelector('.answer')
     accord.addEventListener('click', ()=>{
         icons.classList.toggle('active')
         answers.classList.toggle('active')
     })
-    
 })
+
+
