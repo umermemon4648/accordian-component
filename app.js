5a0fad4f451387f1a9fd7a7e993bf141b453f265
umermemon4648 <umermemon4648@gmail.com>
2023-05-11 07:32:13 +0500

    styling done

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("Hello")
+const allAccordian = document.querySelectorAll('.accordin')
+const icons =  document.querySelector('.icons')
+const answers =  document.querySelector('.answer')
+allAccordian.forEach((accord)=>{
+
+
+})
