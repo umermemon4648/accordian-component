2c96d3d7e092fa4cd3e842d2bb09fdae604be990
umermemon4648 <umermemon4648@gmail.com>
2023-05-11 14:50:56 +0500

    accordian feature completed

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// method 1 (with smooth transition)
 const allAccordian = document.querySelectorAll('.accordian')
 allAccordian.forEach(accord =>{
     const icons =  accord.querySelector('.icons')
     const answers =  accord.querySelector('.answer')
     accord.addEventListener('click', ()=>{
-        icons.classList.toggle('active')
-        answers.classList.toggle('active')
+        if (icons.classList.contains('active')) {
+            icons.classList.remove('active')
+            answers.style.maxHeight = null
+        } else {
+            icons.classList.add('active')
+            answers.style.maxHeight = answers.scrollHeight + "px"
+            
+        }
     })
 })
 
+
+// method 2
+
+// const allAccordian = document.querySelectorAll('.accordian')
+// allAccordian.forEach(accord =>{
+//     const icons =  accord.querySelector('.icons')
+//     const answers =  accord.querySelector('.answer')
+//     accord.addEventListener('click', ()=>{
+//         icons.classList.toggle('active')
+//         answers.classList.toggle('active')
+        
+//     })
+// })
 
